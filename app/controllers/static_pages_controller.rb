cf57e446f0b4647a42bf797fcc11caeaf068ea7c
Davin Lin <lindavin@grinnell.edu>
2020-10-13 22:11:45 -0400

    Remove setting up glossary with the database

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,13 +7,6 @@ class StaticPagesController < ApplicationController
     ## https://stackoverflow.com/questions/1826727/how-do-i-parse-json-with-ruby-on-rails
     @data = File.read("#{Rails.root}/public/glossary.json");
     @glossary_sheet = JSON.parse(@data).sort_by { |k, v| k };
-    @word = Word.new
-    @test_glossary = [
-      {word: 'community-party', content: "<h4> Here we store what a community party might mean...</h4>"},
-      {word: 'community-event', content: "<h4> Similarly we store what a community even could mean... </h4>"},
-      {word: 'community host', content: "<em> This is about hosting events for the community </em>"}
-  ].sort_by {|w| w[:term]}
-    @glossary = Word.order(:word)
   end
 
   def search
